20f5d52220f115d2588f49adc821d71ae1ba06e3
funnker <67109377+funnker@users.noreply.github.com>
2021-04-22 19:00:22 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,107 @@
+//NavBar
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  //Toggle Nav
+  burger.addEventListener('click', () => {
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    //Animate Links
+    navLinks.forEach((link, index)=>{
+      if(link.style.animation){
+        link.style.animation = ''
+      }
+      else
+      {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    })
+    burger.classList.toggle('toggleCB');
+  });
+}
+navSlide();
+
+//TypeWriter
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [" Cristi", " a developer", " a student"];
+const typingDelay = 200; // Typing Delay
+const erasingDelay = 100; // Erasing Delay
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+//jQuery Smooth Scroll
+// $('.navigation a').on('click', function(e) {
+//     if(this.hash !== '')
+//     {
+//         e.preventDefault();
+
+//         const hash = this.hash;
+
+//         $('html, body').animate(
+//             {
+//                 scrollTop: $(hash).offset().top
+//             },
+//             800
+//         );
+//     }
+// })
+
+//Top Button
+
+mybutton = document.getElementById("topButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if(window.location.pathname !== '/index.html#home' || window.location.pathname !== '/index.html')
+  {
+    mybutton.style.display = "block";
+  }
+  else
+  {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, IE, Opera
+}
